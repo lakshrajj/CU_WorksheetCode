f408135f4f5c5e9884f05a78a2f5d46e8af023c2
Luxx <70052551+lakshrajj@users.noreply.github.com>
2023-04-10 10:23:48 +0530

    Create Main - Kotlin.kt

---
+++ MAD/2_2/Main - Kotlin.kt
@@ -0,0 +1,28 @@
+
+import android.os.Bundle
+import android.telephony.SmsManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var phoneEdt: EditText
+    lateinit var messageEdt: EditText
+    lateinit var sendMsgBtn: Button
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        phoneEdt = findViewById(R.id.phoneEdt)
+        messageEdt = findViewById(R.id.messageEdt)
+        sendMsgBtn = findViewById(R.id.sendMsgBtn)
+
+        sendMsgBtn.setOnClickListener {
+            val smsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phoneEdt.text.toString(), null, messageEdt.text.toString(), null, null) }
+
+}}
