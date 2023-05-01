4ca2e81e3ef81e6bd382f5301c143e3e203ea083
Luxx <70052551+lakshrajj@users.noreply.github.com>
2023-05-01 10:50:18 +0530

    Create Main.kt
    Main Activity

---
+++ MAD/EXP8/Main.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonl: Button = findViewById(R.id.button7)
+        val buttonr: Button = findViewById(R.id.button8)
+        val buttona: Button = findViewById(R.id.button9)
+
+        buttonl.setOnClickListener {
+            val intent = Intent(this, LinearLayout::class.java)
+            startActivity(intent)
+        }
+        buttonr.setOnClickListener {
+            val intent = Intent(this, RelativeLayout::class.java)
+            startActivity(intent)
+        }
+        buttona.setOnClickListener {
+            val intent = Intent(this, AbsoluteLayout::class.java)
+            startActivity(intent)
+        }
+    }
+}
